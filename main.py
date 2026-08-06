c0964b7fdecc57dd363c1c9bdebcf5176ed912b7
Maximilian Eventov <292918321+m4xv3@users.noreply.github.com>
2026-08-06 08:00:10 +0000

    Create CentralBank class

--- main.py
+++ main.py
@@ -1,3 +1,12 @@
 class Economy:
     def __init__(self):
         pass
+
+class CentralBank:
+    def __init__(self):
+        self.inflation_target = 0.02
+        self.policy_interest_rate = 0.03
+        self.policy_response_strength = 0.5
+        self.inflation_tolerance = 0.005
+        self.independence = 0.5
+        self.policy_stance = "neutral"
