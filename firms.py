a2c7e4afbc26278b441ce3f28bd15f61d5326684
Maximilian Eventov <292918321+m4xv3@users.noreply.github.com>
2026-09-21 10:14:18 +0000

    Add Firm class with initialization for production, workforce, prices, revenue, and demand

---
+++ firms.py
@@ -0,0 +1,6 @@
+class Firm:
+    def __init__(self, production, num_workers, prices, revenue, demand):
+        self.production = production
+        self.num_workers = num_workers
+        self.prices = prices
+        self.demand = demand
